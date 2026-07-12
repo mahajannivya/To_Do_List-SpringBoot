03afa187c9d8fa2e6a8e5bd818e4f5ef07478d01
nivya <mahajannivya@gmail.com>
2026-07-12 18:26:00 +0530

    Made the entity todo for identity, task name and completion boolean variable

---
+++ src/main/java/To_do_list/entity/ToDo.java
@@ -0,0 +1,18 @@
+package To_do_list.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class ToDo {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+private String task;
+
+private boolean completed;
+}
