20aa3b5429d89e637a9d270f967f253f6bce8911
nivya <mahajannivya@gmail.com>
2026-07-12 18:51:45 +0530

    Added methods for adding a task and for getting all tasks

--- src/main/java/To_do_list/service/ToDoService.java
+++ src/main/java/To_do_list/service/ToDoService.java
@@ -1,7 +1,10 @@
 package To_do_list.service;
 
+import To_do_list.entity.ToDo;
 import To_do_list.repository.ToDoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ToDoService {
@@ -10,5 +13,14 @@ public class ToDoService {
 
     public ToDoService(ToDoRepository repository){
         this.repository = repository;
+    }
+    public List<ToDo> getAllTasks(){
+
+        return repository.findAll();
+    }
+
+
+    public void addTask(ToDo todo){
+        repository.save(todo);
     }
 }
