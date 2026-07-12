76d9a92e232ec4da1f6b811f005fb11df7988d7e
nivya <mahajannivya@gmail.com>
2026-07-12 18:30:45 +0530

    Added Getters and Setters for the task variables

--- src/main/java/To_do_list/entity/ToDo.java
+++ src/main/java/To_do_list/entity/ToDo.java
@@ -15,4 +15,28 @@ private Long id;
 private String task;
 
 private boolean completed;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
