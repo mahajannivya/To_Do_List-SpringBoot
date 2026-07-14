7ca759462d515c958a394a0a8a49b9233455b39e
nivya <mahajannivya@gmail.com>
2026-07-14 16:54:29 +0530

    Added add task controller method

--- src/main/java/To_do_list/controller/ToDoController.java
+++ src/main/java/To_do_list/controller/ToDoController.java
@@ -5,6 +5,8 @@ import To_do_list.service.ToDoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ToDoController {
@@ -26,6 +28,13 @@ public class ToDoController {
         );
 
         return "index";
+    }
+    @PostMapping("/add")
+    public String addTask(@ModelAttribute ToDo todo){
+
+        service.addTask(todo);
+
+        return "redirect:/";
     }
 
 }
