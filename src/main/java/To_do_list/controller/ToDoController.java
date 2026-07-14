53fc7a6f026cbde7ea827622a3c1a06192f4f986
nivya <mahajannivya@gmail.com>
2026-07-14 16:44:46 +0530

    connected home page with controller

--- src/main/java/To_do_list/controller/ToDoController.java
+++ src/main/java/To_do_list/controller/ToDoController.java
@@ -1,7 +1,10 @@
 package To_do_list.controller;
 
+import To_do_list.entity.ToDo;
 import To_do_list.service.ToDoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ToDoController {
@@ -9,4 +12,20 @@ public class ToDoController {
     public ToDoController(ToDoService service){
         this.service=service;
     }
+    @GetMapping("/")
+    public String home(Model model){
+
+        model.addAttribute(
+                "tasks",
+                service.getAllTasks()
+        );
+
+        model.addAttribute(
+                "todo",
+                new ToDo()
+        );
+
+        return "index";
+    }
+
 }
