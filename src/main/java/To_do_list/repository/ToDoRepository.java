cd69a60a6aa659ebe7c0fc64cfc1f9385f537f7c
nivya <mahajannivya@gmail.com>
2026-07-15 12:27:25 +0530

    Updated To-Do List project

--- src/main/java/To_do_list/repository/ToDoRepository.java
+++ src/main/java/To_do_list/repository/ToDoRepository.java
@@ -2,6 +2,8 @@ package To_do_list.repository;
 
 import To_do_list.entity.ToDo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ToDoRepository extends JpaRepository<ToDo,Long> {
 }
