d7633d8e57ac389144e6b021e12505985880640a
nivya <mahajannivya@gmail.com>
2026-07-16 12:10:43 +0530

    made api function for adding a task

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -6,3 +6,27 @@ const form = document.getElementById("todoForm");
 
 
 window.onload = loadTasks;
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const task = taskInput.value.trim();
+
+    if (task === "") return;
+
+    await fetch(API, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            task: task,
+            completed: false
+        })
+    });
+
+    taskInput.value = "";
+
+    loadTasks();
+});
